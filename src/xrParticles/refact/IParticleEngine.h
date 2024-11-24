2fdd18df49712bd8077042efeb778c1dada7f549
Alexander Akkur <akkur.software@gmail.com>
2024-11-24 14:42:21 +0600

    Preparation the particle engine

--- src/xrParticles/refact/IParticleEngine.h
+++ src/xrParticles/refact/IParticleEngine.h
@@ -1,36 +1,33 @@
 #pragma once
 #include "../../xrCore/xrCore.h"
-#include "ParticleActionType.h"
+#include "ParticleActionBase.h"
 #include "ParticleCallbacks.h"
-
-struct ParticleAction;
 
 /// <summary>
 /// 
 /// </summary>
 __interface IParticleEngine
-{
-	int CreateEffect(u32 maxParticleCount);
-	void DestroyEffect(int effectId);
+{	
+	int CreateEffect(uint32_t maxParticleCount);
+	void DestroyEffect(const int32_t& effectId);
 
 	int CreateActionList();
-	void DestroyActionList(int actionListId);
+	void DestroyActionList(const int32_t& actionListId);
 
-	void PlayEffect(int effectId, int actionListId);
-	void StopEffect(int effectId, int actionListId, bool isDeffered = true);
+	void PlayEffect(const int32_t& effectId, const int32_t& actionListId);
+	void StopEffect(const int32_t& effectId, const int32_t& actionListId, const bool& isDeffered = true);
 
-	void Update(int effectId, int actionListId, float dt);
-	void Render(int effectId);
-	void Transform(int actionListId, const Fmatrix& matrix, const Fvector& velocity);
+	void Update(const int32_t& effectId, const int32_t& actionListId, const float& delta);
+	void Transform(const int32_t& actionListId, const Fmatrix& matrix, const Fvector& velocity);
 
-	void RemoveParticle(int effectId, u32 particleId);
-	void SetMaxParticles(int effectId, u32 maxParticleCount);
-	void SetBirthCallback(int effectId, OnBirthParticle b, void* owner, u32 param);
-	void SetDeathCallback(int effectId, OnDeadParticle b, void* owner, u32 param);
-	void GetParticles(int effectId, Particle*& particles, u32& count);
-	u32 GetParticlesCount(int effectId);
+	void RemoveParticle(const int32_t& effectId, const uint32_t& particleId);
+	void SetMaxParticles(const int32_t& effectId, const uint32_t& maxParticleCount);
+	void SetBirthCallback(const int32_t& effectId, const OnBirthParticle& callback, void* owner, const uint32_t& param);
+	void SetDeathCallback(const int32_t& effectId, const OnDeadParticle& callback, void* owner, const uint32_t& param);
+	void GetParticles(const int32_t& effectId, const uint32_t& count);
+	size_t GetParticlesCount(const int32_t& effectId);
 
-	ParticleAction* CreateAction(ParticleActionType type);
-	u32 LoadActions(int actionListId, IReader& reader);
-	void SaveActions(int actionListId, IWriter& writer);
+	ParticleActionBase* CreateEmptyAction(const ParticleActionType& type);
+	uint32_t LoadActions(const int32_t& actionListId, IReader& reader);
+	void SaveActions(const int32_t& actionListId, IWriter& writer);
 };
